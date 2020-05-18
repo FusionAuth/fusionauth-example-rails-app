6fc869697239f5ab0faf7460c06f689df51cf19b
Dan Moore <github@mooreds.com>
2020-05-18 22:43:27 +0000

    Added header for authentication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,19 +3,19 @@ class WelcomeController < ApplicationController
     @signed_in = false
     if cookies[:myjwt] 
       token = cookies[:myjwt]
-      @decoded_token = JWT.decode token, Rails.configuration.x.oauth.jwt_secret, true, { algorithm: 'HS256' }
-      @res = RestClient.get('http://localhost:3000/messages', headers={})
-      @signed_in = true
+      begin
+        @decoded_token = JWT.decode token, Rails.configuration.x.oauth.jwt_secret, true, { algorithm: 'HS256' }
+        @res = RestClient.get('http://localhost:3000/messages', {AUTHORIZATION: 'Bearer '+token })
+        @signed_in = true
+      rescue JWT::DecodeError
+        @signed_in = false
+        @authurl = build_auth_url
+      end
     else 
-      client_id = Rails.configuration.x.oauth.client_id = '0ef466ba-0505-4150-9b34-da0895a98cae'
-      client_secret = Rails.configuration.x.oauth.client_secret = 'hBWwAt32AQlfXV_pm93dkAFxd51WJhcPpfFp5_xZN6I'
-      idp_address = Rails.configuration.x.oauth.idp_address = 'http://localhost:9011/'
-  
-      client = OAuth2::Client.new(client_id, client_secret, site: idp_address, authorize_url: '/oauth2/authorize')
-      redirect_uri = 'http://localhost:4000/oauth2-callback'
-      @authurl = client.auth_code.authorize_url(redirect_uri: redirect_uri)
+      @authurl = build_auth_url
     end
   end
+
   def oauthcallback
     client_id = Rails.configuration.x.oauth.client_id = '0ef466ba-0505-4150-9b34-da0895a98cae'
     client_secret = Rails.configuration.x.oauth.client_secret = 'hBWwAt32AQlfXV_pm93dkAFxd51WJhcPpfFp5_xZN6I'
@@ -27,5 +27,14 @@ class WelcomeController < ApplicationController
     token = client.auth_code.get_token(params[:code], 'redirect_uri': redirect_uri)
     cookies[:myjwt] = { value: token.to_hash[:access_token], httponly: true }
     redirect_to root_path
+  end
+  def build_auth_url
+    client_id = Rails.configuration.x.oauth.client_id = '0ef466ba-0505-4150-9b34-da0895a98cae'
+    client_secret = Rails.configuration.x.oauth.client_secret = 'hBWwAt32AQlfXV_pm93dkAFxd51WJhcPpfFp5_xZN6I'
+    idp_address = Rails.configuration.x.oauth.idp_address = 'http://localhost:9011/'
+ 
+    client = OAuth2::Client.new(client_id, client_secret, site: idp_address, authorize_url: '/oauth2/authorize')
+    redirect_uri = 'http://localhost:4000/oauth2-callback'
+    client.auth_code.authorize_url(redirect_uri: redirect_uri)
   end
 end
